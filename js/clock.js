331bb5d517d788946543bcae455db6a761cc577c
jaehyeon Ye <forthevision7@gmail.com>
2022-01-21 16:22:27 +0900

    Create clock.js

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+//clock.innerText = "lalala";
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    //clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    //console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+}
+
+getClock();
+setInterval(getClock, 1000);
+//setTimeout(sayHello, 5000);
