1b2c1ca7af6ad41b2cd84245fba3881da15f8cf8
jaehyeon Ye <forthevision7@gmail.com>
2022-01-25 10:18:05 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,92 +1,88 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
-//const greeting = document.querySelector("#greeting");
-const greeting = document.querySelector("h1#greeting");
-//const loginButton = document.querySelector("#login-form button");
+const logoutForm = document.querySelector("#logout-form");
+const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+function loginSubmit(event) {
+event.preventDefault();
+const username = loginInput.value;
+loginForm.classList.add(HIDDEN_CLASSNAME);
+logoutForm.classList.remove(HIDDEN_CLASSNAME);
+localStorage.setItem(USERNAME_KEY, username);
+paintGreetings(username);
+}
+
+function paintGreetings(username) {
+greeting.innerText = `Hello! ${username}`;
+greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function logoutBtn() {
+localStorage.removeItem(USERNAME_KEY);
+location.reload();
+}
+
+logoutForm.addEventListener("click", logoutBtn);
+//
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+loginForm.addEventListener("submit", loginSubmit);
+} else {
+paintGreetings(savedUsername);
+}
 /*
-function onLoginSubmit(event){ //paintGreetings에 username으로 넘기지 않는 경우
-    event.preventDefault();
-    //const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    //const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, loginInput.value); //removeItem, getItem
-    //console.log(username);
-    //greeting.innerText = "Hello " + username;
-    paintGreetings();
-    /*
-    greeting.innerText = `Hello ${username}`; //백틱. 작은 따옴표 ''와 혼동 주의할 것!
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    */
-    //const username = loginInput.value;
-    //console.log(loginInput.value);
-    /*
-    if(username == ""){
-       alert("please write your name.");
-    }
-    else if(username.length > 15){
-        alert("Your name is too long.")
-    }
-    */
-    //console.log(loginInput.value);
-    //console.log("click");
-//}
-
-
+const loginForm = document.querySelector("#login-form");
+const logoutForm = document.querySelector("#logout-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("h1#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME2 = "hidden2";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){  
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    logoutForm.classList.add(HIDDEN_CLASSNAME2);
     const username = loginInput.value;
     //localStorage.setItem(USERNAME_KEY, loginInput.value);
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
 
+function LogoutSubmit(event){
+    logoutForm.addEventListener("submit", logoutGreetings);
+    
+}
 
-//loginForm.addEventListener("submit",onLoginSubmit);
 function paintGreetings(username){
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-/*
-function paintGreetings(){
-    const username = localStorage.getItem(USERNAME_KEY);
-    greeting.innerText = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+function logoutGreetings(username){
+    console.log(`See you again ${username}`);
+    logoutGreetings(username);
 }
-*/
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+logoutForm.classList.add(HIDDEN_CLASSNAME2);
 
 if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
+    //logoutGreetings(savedUsername);
+    logoutForm.classList.remove(HIDDEN_CLASSNAME2);
+    logoutForm.addEventListener("click", LogoutSubmit);
     //show the greetings
 }
-/*
-function handleLinkClick(event){
-    event.preventDefault();
-    console.log(event);
-    alert("clicked");
-}*/
+*/
 
-//link.addEventListener("click", handleLinkClick);
-
-//loginButton.addEventListener("click", LogInButtonClick);
-/*
-const age = parseInt(prompt("How old are you?")); //prompt함수 안에 "asdfasd"인 경우는 string을 number로 변환 못해서 NaN가 된다.
-//parseInt 가 string을 받아 숫자로 바꾸려고 한다.
-//console.log(age, parseInt(age));
-//NaN = Not a Number
-if(isNaN(age)){
-    console.log("please write a number");
-} else if (age < 18){
-    console.log("You are too young.");
-} else {
-    console.log("You can drink");
-}*/
